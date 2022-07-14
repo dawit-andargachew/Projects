653511597c7be60026eebff7ef411b2fe13ce5d1
Dawit Andargachew <dawit1111111@gmail.com>
2022-07-15 01:44:33 +0300

    all crud operations work well ! >> what about beyound that like validation and work with noSql

---
+++ ASP_API/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP_API.Data
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        //public DateTime DateofBirth { set; get; }
+        public string Department { get; set; }
+
+    }
+}
---
+++ ASP_API/Migrations/20220714142015_initial migration -- done.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_API.Migrations
+{
+    public partial class initialmigrationdone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
